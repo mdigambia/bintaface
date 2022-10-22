cfa0f14a98455c100b3dc28482afe416141f9cf1
camariana <camariana40@outlook.com>
2022-10-22 15:49:51 +0000

    added  user routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { createUser } = require('../controllers/user');
+const { createUser, getAllUsers, getOneUser, updateOne, deleteOne } = require('../controllers/user');
 
 
 // User route
 router.post('/user', createUser);
+router.get('/user', getAllUsers);
+router.get('/user/:id', getOneUser);
+router.put('/user/:id', updateOne);
+router.delete('/user/:id', deleteOne);
+
+// User route
+router.post('/bed', createUser);
+router.get('/user', getAllUsers);
+router.get('/user/:id', getOneUser);
+router.put('/user/:id', updateOne);
+router.delete('/user/:id', deleteOne);
 
 
 module.exports = router;
